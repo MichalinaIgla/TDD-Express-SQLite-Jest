557fadca6addf1e417c7e0bb3d26a5985a591f7f
Michalina Igla <michalina.igla@gmail.com>
2021-07-12 00:34:41 +0200

    Refactor and add UserRouter & UserService

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // npx http-server -c-1 -p 8080 -P http://localhost:3000
-
+// "no-unused-vars": [
+//   "warn",
+//   {
+//     "vars": "all",
+//     "args": "after-used",
+//     "ignoreRestSiblings": false
+//   }
+// ]
 const app = require('./src/app');
 const sequelize = require('./src/config/database');
 
