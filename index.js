20e6d45bb997bb417829a43eec566b3f9f49d2a6
Michalina Igla <michalina.igla@gmail.com>
2021-07-11 19:46:58 +0200

    Save user to database SQLite

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// npx http-server -c-1 -p 8080 -P http://localhost:3000
+
 const app = require('./src/app');
+const sequelize = require('./src/config/database');
+
+sequelize.sync();
 
 app.listen(3000, () => console.log('App is running!'));
