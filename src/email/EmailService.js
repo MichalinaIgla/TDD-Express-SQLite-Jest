3cf431dcae240c838e60a62d42c75ef183b85b19
Michalina Igla <michalina.igla@gmail.com>
2021-07-15 09:35:07 +0200

    Testing mail token on Web Client

--- src/email/EmailService.js
+++ src/email/EmailService.js
@@ -1,12 +1,25 @@
+const nodemailer = require('nodemailer');
 const transporter = require('../config/emailTransporter');
 
 const sendAccountActivation = async (email, token) => {
-  await transporter.sendMail({
-    from: 'My app <info@myapp.com',
+  console.log(email, 'emailemailemail');
+  const info = await transporter.sendMail({
+    from: 'My App <info@my-app.com>',
     to: email,
     subject: 'Account activation',
-    html: `Token is ${token}`,
+    html: `
+    <div>
+      <b>Please click below link to activate your account</b>
+    </div>
+    <div>
+      <a href="http://localhost:8080/#/login?token=${token}">click me</a>
+    </div>
+    `,
   });
+  console.log(info, 'emailemailemail');
+  if (process.env.NODE_ENV === 'development') {
+    console.log('url: ' + nodemailer.getTestMessageUrl(info));
+  }
 };
 
 module.exports = { sendAccountActivation };
