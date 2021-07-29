42109d3c6bff44d34bba6972c41c0057c5b3eabf
Michalina Igla <michalina.igla@gmail.com>
2021-07-29 11:22:21 +0200

    Add Authentication Failure translation

--- __test__/Auth.spec.js
+++ __test__/Auth.spec.js
@@ -24,8 +24,12 @@ const addUser = async () => {
   return await User.create(user);
 };
 
-const postAuthentication = async (credentials) => {
-  return await request(app).post('/api/1.0/auth').send(credentials);
+const postAuthentication = async (credentials, options = {}) => {
+  let agent = request(app).post('/api/1.0/auth');
+  if (options.language) {
+    agent.set('Accept-Language', options.language);
+  }
+  return await agent.send(credentials);
 };
 
 describe('Authentication', () => {
@@ -48,4 +52,39 @@ describe('Authentication', () => {
     expect(response.body.username).toBe(user.username);
     expect(Object.keys(response.body)).toEqual(['id', 'username']);
   });
+
+  it('returns 401 when user does not exist', async () => {
+    const response = await postAuthentication({
+      email: 'user1@mail.com',
+      password: 'P4ssword',
+    });
+    expect(response.status).toBe(401);
+  });
+
+  it('returns proper error body when authentication fails', async () => {
+    const nowInMillis = new Date().getTime();
+    const response = await postAuthentication({
+      email: 'user1@mail.com',
+      password: 'P4ssword',
+    });
+    const error = response.body;
+    expect(error.path).toBe('/api/1.0/auth');
+    expect(error.timestamp).toBeGreaterThan(nowInMillis);
+    expect(Object.keys(error)).toEqual(['path', 'timestamp', 'message']);
+  });
+
+  it.each`
+    language | message
+    ${'pl'}  | ${'Nie poprawne dane'}
+    ${'en'}  | ${'Incorrect credentials'}
+  `(
+    'returns $message when authentication fails and language is set as $language',
+    async ({ language, message }) => {
+      const response = await postAuthentication(
+        { email: 'user1@mail.com', password: 'P4ssword' },
+        { language }
+      );
+      expect(response.body.message).toBe(message);
+    }
+  );
 });
--- src/auth/AuthenticationRouter.js
+++ src/auth/AuthenticationRouter.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const UserService = require('../user/UserService');
+const AuthenticationException = require('./AuthenticationException');
 
-router.post('/api/1.0/auth', async (req, res) => {
+router.post('/api/1.0/auth', async (req, res, next) => {
   const { email } = req.body;
   const user = await UserService.findByEmail(email);
+  if (!user) {
+    return next(new AuthenticationException());
+    // return res.status(401).send();
+  }
   res.send({
     id: user.id,
     username: user.username,
