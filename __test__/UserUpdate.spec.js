1108f74d394b589a604ca5e346d6ba86f0ac98da
Michalina Igla <michalina.igla@gmail.com>
2021-07-29 12:27:39 +0200

    Refactor request in User Update Test

--- __test__/UserUpdate.spec.js
+++ __test__/UserUpdate.spec.js
@@ -13,9 +13,17 @@ beforeEach(async () => {
   await User.destroy({ truncate: true });
 });
 
+const putUser = (id = 5, body = null, options = {}) => {
+  const agent = request(app).put('/api/1.0/users/' + id);
+  if (options.language) {
+    agent.set('Accept-Language', options.language);
+  }
+  return agent.send(body);
+};
+
 describe('User update', () => {
   it('returns forbidden when request sent without basic authorization', async () => {
-    const response = await request(app).put('/api/1.0/users/5').send();
+    const response = await putUser();
     expect(response.status).toBe(403);
   });
 
@@ -27,10 +35,7 @@ describe('User update', () => {
     'returns error body with $message for unauthroized request when language is $language',
     async ({ language, message }) => {
       const nowInMillis = new Date().getTime();
-      const response = await request(app)
-        .put('/api/1.0/users/5')
-        .set('Accept-Language', language)
-        .send();
+      const response = await putUser(5, null, { language });
       expect(response.body.path).toBe('/api/1.0/users/5');
       expect(response.body.timestamp).toBeGreaterThan(nowInMillis);
       expect(response.body.message).toBe(message);
