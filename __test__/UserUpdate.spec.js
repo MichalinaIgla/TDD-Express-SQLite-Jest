0a2421402a1f8f0a629378b6c0e50c9c451050fa
Michalina Igla <michalina.igla@gmail.com>
2021-07-29 12:40:50 +0200

    Add Basic Authorisation basic64

--- __test__/UserUpdate.spec.js
+++ __test__/UserUpdate.spec.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const app = require('../src/app');
 const User = require('../src/user/User');
 const sequelize = require('../src/config/database');
+const bcrypt = require('bcrypt');
 const en = require('../src/locales/en/translation.json');
 const pl = require('../src/locales/pl/translation.json');
 
@@ -13,10 +14,31 @@ beforeEach(async () => {
   await User.destroy({ truncate: true });
 });
 
+const activeUser = {
+  username: 'user1',
+  email: 'user1@mail.com',
+  password: 'P4ssword',
+  inactive: false,
+};
+
+const addUser = async (user = { ...activeUser }) => {
+  const hash = await bcrypt.hash(user.password, 10);
+  user.password = hash;
+  return await User.create(user);
+};
+
 const putUser = (id = 5, body = null, options = {}) => {
   const agent = request(app).put('/api/1.0/users/' + id);
   if (options.language) {
     agent.set('Accept-Language', options.language);
+  }
+  if (options.auth) {
+    // Authorization: Basic dxN ...,
+    // const merged = `${email}:${password}`;
+    // const base64 = Buffer.from(merged).toString('base64');
+    // agent.set('Authorization', `Basic ${base64}`);
+    const { email, password } = options.auth;
+    agent.auth(email, password);
   }
   return agent.send(body);
 };
@@ -41,4 +63,12 @@ describe('User update', () => {
       expect(response.body.message).toBe(message);
     }
   );
+
+  it('returns forbidden when request sent incorrect email in basic authorization', async () => {
+    await addUser();
+    const response = await putUser(5, null, {
+      auth: { email: 'user1000@mail.com', password: 'P4ssword' },
+    });
+    expect(response.status).toBe(403);
+  });
 });
