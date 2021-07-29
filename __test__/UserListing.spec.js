4c97f9bbb0b4089aa107b41999f7892ebe0bd973
Michalina Igla <michalina.igla@gmail.com>
2021-07-29 15:18:09 +0200

    Add Basic Auth in get Users

--- __test__/UserListing.spec.js
+++ __test__/UserListing.spec.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const app = require('../src/app');
 const User = require('../src/user/User');
 const sequelize = require('../src/config/database');
+const bcrypt = require('bcrypt');
 const en = require('../src/locales/en/translation.json');
 const pl = require('../src/locales/pl/translation.json');
 
@@ -13,16 +14,23 @@ beforeEach(async () => {
   await User.destroy({ truncate: true });
 });
 
-const getUsers = () => {
-  return request(app).get('/api/1.0/users');
+const getUsers = (options = {}) => {
+  const agent = request(app).get('/api/1.0/users');
+  if (options.auth) {
+    const { email, password } = options.auth;
+    agent.auth(email, password);
+  }
+  return agent;
 };
 
 const addUsers = async (activeUserCount, inactiveUserCount = 0) => {
+  const hash = await bcrypt.hash('P4ssword', 10);
   for (let i = 0; i < activeUserCount + inactiveUserCount; i++) {
     await User.create({
       username: `user${i + 1}`,
       email: `user${i + 1}@mail.com`,
       inactive: i >= activeUserCount,
+      password: hash,
     });
   }
 };
@@ -108,6 +116,13 @@ describe('Listing Users', () => {
     expect(response.body.content.length).toBe(10);
     expect(response.body.size).toBe(10);
     expect(response.body.page).toBe(0);
+  });
+  it('returns user page without logged in user when request has valid authorization', async () => {
+    await addUsers(11);
+    const response = await getUsers({
+      auth: { email: 'user1@mail.com', password: 'P4ssword' },
+    });
+    expect(response.body.totalPages).toBe(1);
   });
 });
 
